5c77c32beb38d6e82095dd1eea768bb5270172d7
FusionZhu <tianlangstudio@aliyun.com>
2025-06-13 20:01:00 +0800

    try to fix vercel error

--- src/lib/tutorial/adapters/common/index.js
+++ src/lib/tutorial/adapters/common/index.js
@@ -1,12 +1,8 @@
-import zipped from './common.zip?url';
 
 async function load() {
-	const result = await Promise.all([
-		fetch(zipped).then((r) => r.arrayBuffer())
-	]);
 
 	return {
-		zipped: result[0],
+		zipped: '',
 		unzip: ''
 	};
 }
